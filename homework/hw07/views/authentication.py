4532cffc821eafdf026241cf8f0cdd8b97428889
Jazz <jcronin@unca.edu>
2023-04-30 19:12:50 -0400

    fixing

--- homework/hw07/views/authentication.py
+++ homework/hw07/views/authentication.py
@@ -2,20 +2,46 @@ from flask import request, \
     make_response, render_template, redirect
 from models import User
 import flask_jwt_extended
+import datetime
+
 
 def logout():
     # # Uncomment these lines to delete the cookies and
     # # redirect the user to the login screen
     
-    # response = make_response(redirect('/login', 302))
-    # flask_jwt_extended.unset_jwt_cookies(response)
-    # return response
-    return 'TODO: Logout'
+    response = make_response(redirect('/login', 302))
+    flask_jwt_extended.unset_jwt_cookies(response)
+    return response
+    #return 'TODO: Logout'
 
 def login():
     if request.method == 'POST':
-        print('See lecture 25 video + starter files')
-        return 'See lecture 25 video + starter files'
+        print(request.form)
+        username =request.form.get('username')
+        password = request.form.get('password')
+        #print('See lecture 25 video + starter files')
+        #return 'See lecture 25 video + starter files'
+        user = User.query.filter_by(username=username).pass_test()
+        if user is None:
+            return render_template(
+                'login.html',
+                message = 'not found'
+            )
+        if user and not user.password_checker(password):
+            return render_template(
+                'login.html',
+                message = 'incorrect'
+
+            )
+        chips_ahoy = datetime.timedelta(minutes=20)
+        access_token = flask_jwt_extended.create_access_token(
+            identity=user.id, 
+            expires_delta=chips_ahoy
+        )
+        response = make_response(redirect('/', 302))
+        flask_jwt_extended.set_access_cookies(response, access_token)
+        return response
+
     else:
         return render_template(
             'login.html'
--- homework/hw07/views/bookmarks.py
+++ homework/hw07/views/bookmarks.py
@@ -1,8 +1,10 @@
 from flask import Response, request
 from flask_restful import Resource
-from models import Bookmark, db
+from models import Bookmark, db,Post
 import json
 import flask_jwt_extended
+from views import get_authorized_user_ids
+
 
 class BookmarksListEndpoint(Resource):
 
@@ -24,16 +26,16 @@ class BookmarksListEndpoint(Resource):
         try:
             post_id = int(body.get('post_id'))
         except:
-            return Response(json.dumps({'error': 'post_id format incorrect'}), status=400)        
+            return Response(json.dumps({'error': 'invalid'}), status=400)        
 
 
         post = Post.query.get(post_id)
         authorized_ids = get_authorized_user_ids(current_user=self.current_user)
         
         if post == None:
-            return Response(json.dumps({ "error": "post_id not valid" }), mimetype="application/json", status=404)
+            return Response(json.dumps({ "error": " not valid" }), mimetype="application/json", status=404)
         if post.user_id not in authorized_ids:
-            return Response(json.dumps({ "error": "post_id not valid or is unauthorized" }), mimetype="application/json", status=404)
+            return Response(json.dumps({ "error": " not valid" }), mimetype="application/json", status=404)
         new_bookmark = Bookmark(
             user_id=self.current_user.id,
             post_id=body.get('post_id'),
@@ -42,7 +44,7 @@ class BookmarksListEndpoint(Resource):
             db.session.add(new_bookmark)
             db.session.commit()
         except:
-            return Response(json.dumps({ "error": "duplicate post_id" }), mimetype="application/json", status=400)
+            return Response(json.dumps({ "error": "error" }), mimetype="application/json", status=400)
 
         return Response(json.dumps(new_bookmark.to_dict()), mimetype="application/json", status=201)
 
@@ -60,7 +62,7 @@ class BookmarkDetailEndpoint(Resource):
         try:
             id = int(id)
         except:
-            return Response(json.dumps({'error': 'bookmark_id format incorrect'}), status=404)
+            return Response(json.dumps({'error': 'format incorrect'}), status=404)
 
         bookmark = Bookmark.query.get(id)
 
